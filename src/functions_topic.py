dfd9b2f67182691040b42bdfd649122dc52a7bd9
James Alexander Hughes <jameshughes89@Gmail.com>
2022-03-09 12:10:36 -0400

    switch the docstring style to what pycharm says

--- src/functions_topic.py
+++ src/functions_topic.py
@@ -7,9 +7,11 @@ def concatenate_strings(string1: str, string2: str) -> str:
     E.g.
         concatenate_strings("hello", "world") -> "helloworld"
 
-    :param string1: First string of the concatenation
-    :param string2: Second string of the concatenation
-    :return: The concatenation of string1 and string2
+    @rtype: str
+    @param string1: First string of the concatenation
+    @param string2: Second string of the concatenation
+    @return: The concatenation of string1 and string2
+
     """
     concatenated = string1 + string2
     return concatenated
@@ -25,11 +27,12 @@ def criss_cross_concatenation(string1: str, string2: str, string3: str, string4:
     E.g.
         criss_cross_concatenation("hello", "world", "CSCI", "161") -> "helloCSCIworld161"
 
-    :param string1: First string of the concatenation
-    :param string2: Second string of the concatenation (that should be the third one on output)
-    :param string3: Third string of the concatenation (that should be the second one on the output)
-    :param string4: Fourth string of the concatenation
-    :return: The concatenation of string1 string3 string2 string4
+    @rtype: str
+    @param string1: First string of the concatenation
+    @param string2: Second string of the concatenation (that should be the third one on output)
+    @param string3: Third string of the concatenation (that should be the second one on the output)
+    @param string4: Fourth string of the concatenation
+    @return: The concatenation of string1 string3 string2 string4
     """
     first_third = concatenate_strings(string1, string3)
     second_fourth = concatenate_strings(string2, string4)
