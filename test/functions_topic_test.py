1b3e1026bfc0122933a61c7467e47ccc31e2aab1
James Alexander Hughes <jameshughes89@Gmail.com>
2022-03-09 18:48:22 -0400

    ditch param tests

--- test/functions_topic_test.py
+++ test/functions_topic_test.py
@@ -2,20 +2,17 @@ import pytest
 from src.functions_topic import *
 
 
-@pytest.mark.parametrize("string1, string2, expected",
-                         [("", "", ""),
-                          (" ", " ", "  "),
-                          ("a", "b", "ab"),
-                          ("Hello", "World", "HelloWorld")])
-def test_concatenate_strings_returns_concatenated_string(string1, string2, expected):
-    assert concatenate_strings(string1, string2) == expected
+def test_concatenate_strings_empty_strings_returns_empty_string():
+    assert "" == concatenate_strings("", "")
 
 
-@pytest.mark.parametrize("string1, string2, string3, string4, expected",
-                         [("", "", "", "", ""),
-                          (" ", " ", " ", " ", "    "),
-                          ("a", "b", "c", "d", "acbd"),
-                          ("This", "Is", "A", "Test", "ThisAIsTest")])
-def test_criss_cross_concatenation_returns_concatenated_string_in_correct_order(string1, string2, string3, string4,
-                                                                                expected):
-    assert criss_cross_concatenation(string1, string2, string3, string4) == expected
+def test_concatenate_strings_returns_contatenated_strings():
+    assert "ab" == concatenate_strings("a", "b")
+
+
+def test_criss_cross_concatenation_empty_strings_returns_empty_string():
+    assert "" == criss_cross_concatenation("", "", "", "")
+
+
+def test_criss_cross_concatenation_returns_contatenated_strings_in_correct_order():
+    assert "acbd" == criss_cross_concatenation("a", "b", "c", "d")
