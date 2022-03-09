1ce5f9d2427af9fd37666d4b823b6f3c548a2b45
James Alexander Hughes <jameshughes89@Gmail.com>
2022-03-09 13:05:41 -0400

    cris cross test

--- test/functions_topic_test.py
+++ test/functions_topic_test.py
@@ -7,5 +7,15 @@ from src.functions_topic import *
                           (" ", " ", "  "),
                           ("a", "b", "ab"),
                           ("Hello", "World", "HelloWorld")])
-def test_absolute_value_returns_correct_value(string1, string2, expected):
+def test_concatenate_strings_returns_concatenated_string(string1, string2, expected):
     assert concatenate_strings(string1, string2) == expected
+
+
+@pytest.mark.parametrize("string1, string2, string3, string4, expected",
+                         [("", "", "", "", ""),
+                          (" ", " ", " ", " ", "    "),
+                          ("a", "b", "c", "d", "acbd"),
+                          ("This", "Is", "A", "Test", "ThisAIsTest")])
+def test_criss_cross_concatenation_returns_concatenated_string_in_correct_order(string1, string2, string3, string4,
+                                                                                expected):
+    assert criss_cross_concatenation(string1, string2, string3, string4) == expected
