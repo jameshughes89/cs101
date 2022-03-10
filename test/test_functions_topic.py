578595ba28d134c5139d8dd146d2b584be7cae97
James Alexander Hughes <jameshughes89@Gmail.com>
2022-03-10 12:47:31 -0400

    ugh formatting

--- test/test_functions_topic.py
+++ test/test_functions_topic.py
@@ -4,7 +4,6 @@ from src.functions_topic import *
 
 
 class TestFunctionsTopic(unittest.TestCase):
-
     def test_concatenate_strings_empty_strings_returns_empty_string(self):
         self.assertEqual("", concatenate_strings("", ""))
 
@@ -14,5 +13,7 @@ class TestFunctionsTopic(unittest.TestCase):
     def test_criss_cross_concatenation_empty_strings_returns_empty_string(self):
         self.assertEqual("", criss_cross_concatenation("", "", "", ""))
 
-    def test_criss_cross_concatenation_returns_contatenated_strings_in_correct_order(self):
+    def test_criss_cross_concatenation_returns_contatenated_strings_in_correct_order(
+        self,
+    ):
         self.assertEqual("acbd", criss_cross_concatenation("a", "b", "c", "d"))
