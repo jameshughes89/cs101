05226c2e86684d4d4175e8faf2329ed592fe447d
James Alexander Hughes <jameshughes89@Gmail.com>
2022-03-10 10:22:18 -0400

    remove pytest and use unittest

--- test/functions_topic_test.py
+++ test/functions_topic_test.py
@@ -1,18 +1,17 @@
-import pytest
+import unittest
 from src.functions_topic import *
 
 
-def test_concatenate_strings_empty_strings_returns_empty_string():
-    assert "" == concatenate_strings("", "")
+class TestFunctionsTopic(unittest.TestCase):
 
+    def test_concatenate_strings_empty_strings_returns_empty_string(self):
+        self.assertEqual("", concatenate_strings("", ""))
 
-def test_concatenate_strings_returns_contatenated_strings():
-    assert "ab" == concatenate_strings("a", "b")
+    def test_concatenate_strings_returns_contatenated_strings(self):
+        self.assertEqual("ab", concatenate_strings("a", "b"))
 
+    def test_criss_cross_concatenation_empty_strings_returns_empty_string(self):
+        self.assertEqual("", criss_cross_concatenation("", "", "", ""))
 
-def test_criss_cross_concatenation_empty_strings_returns_empty_string():
-    assert "" == criss_cross_concatenation("", "", "", "")
-
-
-def test_criss_cross_concatenation_returns_contatenated_strings_in_correct_order():
-    assert "acbd" == criss_cross_concatenation("a", "b", "c", "d")
+    def test_criss_cross_concatenation_returns_contatenated_strings_in_correct_order(self):
+        self.assertEqual("acbd", criss_cross_concatenation("a", "b", "c", "d"))
