5b7443f9ec588fff7af4626bc3b935aa8675c234
James Alexander Hughes <jameshughes89@Gmail.com>
2022-03-10 10:48:09 -0400

    :new: Start Function Topic (#112)

---
+++ src/functions_topic.py
@@ -0,0 +1,42 @@
+def concatenate_strings(string1: str, string2: str) -> str:
+    """
+    Returns the concatenation of two strings.
+    E.g.
+        concatenate_strings("hello", "world") -> "helloworld"
+
+    @rtype: str
+    @param string1: First string of the concatenation
+    @param string2: Second string of the concatenation
+    @return: The concatenation of string1 and string2
+    """
+    concatenated = string1 + string2
+    return concatenated
+
+
+# Tests for the concatenate_strings method
+assert "" == concatenate_strings("", "")
+assert "ab" == concatenate_strings("a", "b")
+
+
+def criss_cross_concatenation(string1: str, string2: str, string3: str, string4: str) -> str:
+    """
+    Returns the concatenation of the strings in the order of first third second fourth.
+    E.g.
+        criss_cross_concatenation("hello", "world", "CSCI", "161") -> "helloCSCIworld161"
+
+    @rtype: str
+    @param string1: First string of the concatenation
+    @param string2: Second string of the concatenation (that should be the third one on output)
+    @param string3: Third string of the concatenation (that should be the second one on the output)
+    @param string4: Fourth string of the concatenation
+    @return: The concatenation of string1 string3 string2 string4
+    """
+    first_third = concatenate_strings(string1, string3)
+    second_fourth = concatenate_strings(string2, string4)
+    first_third_second_fourth = concatenate_strings(first_third, second_fourth)
+    return first_third_second_fourth
+
+
+# Tests for the criss_cross_concatenation method
+assert "" == criss_cross_concatenation("", "", "", "")
+assert "acbd" == criss_cross_concatenation("a", "b", "c", "d")
---
+++ test/functions_topic_test.py
@@ -0,0 +1,17 @@
+import unittest
+from src.functions_topic import *
+
+
+class TestFunctionsTopic(unittest.TestCase):
+
+    def test_concatenate_strings_empty_strings_returns_empty_string(self):
+        self.assertEqual("", concatenate_strings("", ""))
+
+    def test_concatenate_strings_returns_contatenated_strings(self):
+        self.assertEqual("ab", concatenate_strings("a", "b"))
+
+    def test_criss_cross_concatenation_empty_strings_returns_empty_string(self):
+        self.assertEqual("", criss_cross_concatenation("", "", "", ""))
+
+    def test_criss_cross_concatenation_returns_contatenated_strings_in_correct_order(self):
+        self.assertEqual("acbd", criss_cross_concatenation("a", "b", "c", "d"))
