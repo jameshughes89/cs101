123c1b5322aa662ed7f4b7c5554151b7069a1a12
James Alexander Hughes <jameshughes89@Gmail.com>
2022-03-11 13:37:33 -0400

    update sphinx

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
             "mdformat==0.7.13",
             "mdformat-gfm==0.3.5",
             "mdformat-black==0.1.1",
-            "sphinx==4.0.1",
+            "sphinx==4.4.0",
             "sphinx-rtd-theme==1.0.0",
         ],
         entry_points={"console_scripts": [f"format = setup:{run_code_formatters.__name__}"]},
