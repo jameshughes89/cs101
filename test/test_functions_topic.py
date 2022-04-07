de518c652f6a1e81077115a961bdca94225ec537
James Alexander Hughes <jameshughes89@Gmail.com>
2022-04-07 10:11:50 -0300

    square of sum real tests

--- test/test_functions_topic.py
+++ test/test_functions_topic.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.functions_topic import concatenate_strings, criss_cross_concatenation, celsius_to_fahrenheit
+from src.functions_topic import concatenate_strings, criss_cross_concatenation, celsius_to_fahrenheit, square_of_sum
 
 
 class TestFunctionsTopic(unittest.TestCase):
@@ -12,6 +12,18 @@ class TestFunctionsTopic(unittest.TestCase):
 
     def test_celsius_to_fahrenheit_float_celsius_values_returns_correct_fahrenheit_value(self):
         self.assertAlmostEqual(89.96, celsius_to_fahrenheit(32.2), 3)
+
+    def test_square_of_sum_positive_values_returns_correct_value(self):
+        self.assertEqual(16, square_of_sum(2, 2))
+
+    def test_square_of_sum_negative_values_returns_correct_value(self):
+        self.assertEqual(16, square_of_sum(-2, -2))
+
+    def test_square_of_sum_mixed_sign_values_returns_correct_value(self):
+        self.assertEqual(0, square_of_sum(2, -2))
+
+    def test_square_of_sum_float_values_returns_correct_value(self):
+        self.assertAlmostEqual(19.36, square_of_sum(2.2, 2.2), 4)
 
     def test_concatenate_strings_empty_strings_returns_empty_string(self):
         self.assertEqual("", concatenate_strings("", ""))
