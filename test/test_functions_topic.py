afd3b9514651ed4637a43631d118dfe9febe1a17
James Alexander Hughes <jameshughes89@Gmail.com>
2022-04-07 10:24:26 -0300

    make use of messages

--- test/test_functions_topic.py
+++ test/test_functions_topic.py
@@ -10,34 +10,56 @@ from src.functions_topic import (
 
 class TestFunctionsTopic(unittest.TestCase):
     def test_celsius_to_fahrenheit_positive_celsius_values_returns_correct_fahrenheit_value(self):
-        self.assertEqual(104, celsius_to_fahrenheit(40))
+        expected = 104
+        actual = celsius_to_fahrenheit(40)
+        self.assertEqual(expected, actual, f"Expected {expected} but was {actual}.")
 
     def test_celsius_to_fahrenheit_negative_celsius_values_returns_correct_fahrenheit_value(self):
-        self.assertEqual(-40, celsius_to_fahrenheit(-40))
+        expected = -40
+        actual = celsius_to_fahrenheit(-40)
+        self.assertEqual(expected, actual, f"Expected {expected} but was {actual}.")
 
     def test_celsius_to_fahrenheit_float_celsius_values_returns_correct_fahrenheit_value(self):
-        self.assertAlmostEqual(89.96, celsius_to_fahrenheit(32.2), 3)
+        expected = 89.96
+        actual = celsius_to_fahrenheit(32.2)
+        self.assertAlmostEqual(expected, actual, 3, f"Expected {expected} but was {actual}.")
 
     def test_square_of_sum_positive_values_returns_correct_value(self):
-        self.assertEqual(16, square_of_sum(2, 2))
+        expected = 16
+        actual = square_of_sum(2, 2)
+        self.assertEqual(expected, actual, f"Expected {expected} but was {actual}.")
 
     def test_square_of_sum_negative_values_returns_correct_value(self):
-        self.assertEqual(16, square_of_sum(-2, -2))
+        expected = 16
+        actual = square_of_sum(-2, -2)
+        self.assertEqual(expected, actual, f"Expected {expected} but was {actual}.")
 
     def test_square_of_sum_mixed_sign_values_returns_correct_value(self):
-        self.assertEqual(0, square_of_sum(2, -2))
+        expected = 0
+        actual = square_of_sum(2, -2)
+        self.assertEqual(expected, actual, f"Expected {expected} but was {actual}.")
 
     def test_square_of_sum_float_values_returns_correct_value(self):
-        self.assertAlmostEqual(19.36, square_of_sum(2.2, 2.2), 4)
+        expected = 19.36
+        actual = square_of_sum(2.2, 2.2)
+        self.assertAlmostEqual(expected, actual, 4, f"Expected {expected} but was {actual}.")
 
     def test_concatenate_strings_empty_strings_returns_empty_string(self):
-        self.assertEqual("", concatenate_strings("", ""))
+        expected = ""
+        actual = concatenate_strings("", "")
+        self.assertEqual(expected, actual, f"Expected {expected} but was {actual}.")
 
     def test_concatenate_strings_returns_contatenated_strings(self):
-        self.assertEqual("ab", concatenate_strings("a", "b"))
+        expected = "ab"
+        actual = concatenate_strings("a", "b")
+        self.assertEqual(expected, actual, f"Expected {expected} but was {actual}.")
 
     def test_criss_cross_concatenation_empty_strings_returns_empty_string(self):
-        self.assertEqual("", criss_cross_concatenation("", "", "", ""))
+        expected = ""
+        actual = criss_cross_concatenation("", "", "", "")
+        self.assertEqual(expected, actual, f"Expected {expected} but was {actual}.")
 
     def test_criss_cross_concatenation_returns_contatenated_strings_in_correct_order(self):
-        self.assertEqual("acbd", criss_cross_concatenation("a", "b", "c", "d"))
+        expected = "acbd"
+        actual = criss_cross_concatenation("a", "b", "c", "d")
+        self.assertEqual(expected, actual, f"Expected {expected} but was {actual}.")
