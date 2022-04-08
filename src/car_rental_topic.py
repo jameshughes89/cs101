ab03e9a2b6c17e954eab25a8a6053a8eb26ddf5a
James Alexander Hughes <jameshughes89@Gmail.com>
2022-04-08 15:33:15 -0300

    avg k

--- src/car_rental_topic.py
+++ src/car_rental_topic.py
@@ -15,3 +15,22 @@ assert 0 == total_kms(0, 0)
 assert 100 == total_kms(0, 100)
 assert -100 == total_kms(100, 0)
 assert 100.5 == total_kms(100.5, 201)
+
+
+def average_kms_per_day(num_days, num_kms):
+    """
+    Calculate the average number of kilometers driven per day
+    over the rental period
+
+    @param num_days: The total number of days the car was rented
+    @param num_kms: The total number of kilometers driven during the rental period
+    @return: The average number of kilometers driven per day
+    """
+
+    return num_kms / num_days
+
+
+assert 0 == average_kms_per_day(1, 0)
+assert 1 == average_kms_per_day(1, 1)
+assert -1 == average_kms_per_day(-1, 1)
+assert 0.5 == average_kms_per_day(3, 1.5)
