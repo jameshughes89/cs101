4bb07b70607d2fcdb3fc5d0c75546bcaf74bb094
James Alexander Hughes <jameshughes89@Gmail.com>
2022-04-08 15:38:51 -0300

    kms above average

--- src/car_rental_topic.py
+++ src/car_rental_topic.py
@@ -34,3 +34,26 @@ assert 0 == average_kms_per_day(1, 0)
 assert 1 == average_kms_per_day(1, 1)
 assert -1 == average_kms_per_day(-1, 1)
 assert 0.5 == average_kms_per_day(3, 1.5)
+
+
+def num_kms_above_average(avg_num_kms: float) -> float:
+    """
+    Calculates the number of kms the renter went over of their daily allowance.
+    We will use the customer's average daily kms.
+
+    @param avg_num_kms: average number of kms driven per day
+    @return: The number of kms over 100 they went (return 0 if it's less than 100)
+    """
+
+    # If the average kms traveled is above 100,
+    # return how much above, otherwise zero
+    if avg_num_kms > 100:
+        return avg_num_kms - 100
+    else:
+        return 0
+
+
+assert 0 == num_kms_above_average(100)
+assert 1 == num_kms_above_average(101)
+assert 0 == num_kms_above_average(99)
+assert 100 == num_kms_above_average(200)
