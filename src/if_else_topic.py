52e5d0cbfffceaf3215e40480daaf11132f8404c
James Alexander Hughes <jameshughes89@Gmail.com>
2022-04-26 15:47:27 -0300

    smush function in script

---
+++ src/if_else_topic.py
@@ -0,0 +1,13 @@
+def smush(a_number: float) -> float:
+    """
+    Returns half the value of the parameter a_number if the value is positive,
+    otherwise, return the value of a_number.
+
+    :rtype: float
+    :param a_number: Some arbitrary number.
+    :return: Half of a_number when it is positive, a_number when not positive.
+    """
+    return_value = a_number
+    if a_number > 0:
+        return_value = return_value / 2
+    return return_value
