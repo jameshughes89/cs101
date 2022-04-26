5eed740947b32f5604e2d48f7f690e037adfaeef
James Alexander Hughes <jameshughes89@Gmail.com>
2022-04-26 16:10:21 -0300

    add is_negative to script

--- src/if_else_topic.py
+++ src/if_else_topic.py
@@ -17,3 +17,39 @@ def smush(a_number: float) -> float:
 assert 5 == smush(10)
 assert -10 == smush(-10)
 assert 0 == smush(0)
+
+
+def is_negative(a_number: float) -> bool:
+    """
+    Checks if the provided number is negative or not. Returns True if a_number
+    is negative, False otherwise.
+
+    :rtype: float
+    :param a_number: Some arbitrary number.
+    :return: True if a_number is negative, False otherwise.
+    """
+    if a_number < 0:
+        return True  # Remember, once a return statement is run the function stops
+    return False
+
+
+assert False == is_negative(10)
+assert True == is_negative(-10)
+assert False == is_negative(0)
+
+
+def is_negative_version_2(a_number: float) -> bool:
+    """
+    Checks if the provided number is negative or not. Returns True if a_number
+    is negative, False otherwise.
+
+    :rtype: float
+    :param a_number: Some arbitrary number.
+    :return: True if a_number is negative, False otherwise.
+    """
+    return a_number < 0
+
+
+assert False == is_negative_version_2(10)
+assert True == is_negative_version_2(-10)
+assert False == is_negative_version_2(0)
