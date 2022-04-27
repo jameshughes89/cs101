ea60dc4251035adc4ed64c878dfede469cf6c940
James Alexander Hughes <jameshughes89@Gmail.com>
2022-04-27 08:50:58 -0300

    unittests

--- test/test_if_else_topic.py
+++ test/test_if_else_topic.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.if_else_topic import is_negative, is_negative_version_2, smush
+from src.if_else_topic import is_negative, is_negative_version_2, smush, three_five_divisible
 
 
 class TestIfElseTopic(unittest.TestCase):
@@ -30,3 +30,18 @@ class TestIfElseTopic(unittest.TestCase):
 
     def test_is_negative_version_2_zero_parameter_returns_False(self):
         self.assertFalse(is_negative_version_2(0))
+
+    def test_three_five_divisible_zero_parameter_returns_correct_string(self):
+        self.assertEqual("It is!", three_five_divisible(0))
+
+    def test_three_five_divisible_fifteen_parameter_returns_correct_string(self):
+        self.assertEqual("It is!", three_five_divisible(15))
+
+    def test_three_five_divisible_negative_thirty_parameter_returns_correct_string(self):
+        self.assertEqual("It is!", three_five_divisible(-30))
+
+    def test_three_five_divisible_one_parameter_returns_correct_string(self):
+        self.assertEqual("Nope", three_five_divisible(1))
+
+    def test_three_five_divisible_negative_fourteen_parameter_returns_correct_string(self):
+        self.assertEqual("Nope", three_five_divisible(-14))
