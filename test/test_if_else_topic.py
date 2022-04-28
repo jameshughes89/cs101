13a4681dd96f4aae955f9ef26f92850b0c6dc666
James Alexander Hughes <jameshughes89@Gmail.com>
2022-04-28 12:42:41 -0300

    unit tests

--- test/test_if_else_topic.py
+++ test/test_if_else_topic.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.if_else_topic import is_negative, is_negative_version_2, smush
+from src.if_else_topic import is_negative, is_negative_version_2, smush, hail
 
 
 class TestIfElseTopic(unittest.TestCase):
@@ -30,3 +30,15 @@ class TestIfElseTopic(unittest.TestCase):
 
     def test_is_negative_version_2_zero_parameter_returns_False(self):
         self.assertFalse(is_negative_version_2(0))
+
+    def test_hail_zero_parameter_returns_zero(self):
+        self.assertEqual(0, hail(0))
+
+    def test_hail_even_parameter_returns_half(self):
+        self.assertEqual(1, hail(2))
+
+    def test_hail_odd_parameter_returns_times_three_plus_one(self):
+        self.assertEqual(4, hail(1))
+
+    def test_hail_negative_odd_parameter_returns_times_three_plus_one(self):
+        self.assertEqual(-14, hail(-5))
