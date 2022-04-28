07f96e0a3672d740bdf199be54e257a5f07adb03
James Alexander Hughes <jameshughes89@Gmail.com>
2022-04-28 13:39:55 -0300

    unittests

--- test/test_if_else_topic.py
+++ test/test_if_else_topic.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.if_else_topic import is_negative, is_negative_version_2, smush
+from src.if_else_topic import is_negative, is_negative_version_2, smush, letter_grade
 
 
 class TestIfElseTopic(unittest.TestCase):
@@ -30,3 +30,21 @@ class TestIfElseTopic(unittest.TestCase):
 
     def test_is_negative_version_2_zero_parameter_returns_False(self):
         self.assertFalse(is_negative_version_2(0))
+
+    def test_letter_grade_90_returns_A_plus(self):
+        self.assertEqual("A+", letter_grade(90))
+
+    def test_letter_grade_80_returns_A(self):
+        self.assertEqual("A", letter_grade(80))
+
+    def test_letter_grade_70_returns_B(self):
+        self.assertEqual("B", letter_grade(70))
+
+    def test_letter_grade_60_returns_C(self):
+        self.assertEqual("C", letter_grade(60))
+
+    def test_letter_grade_50_returns_D(self):
+        self.assertEqual("D", letter_grade(50))
+
+    def test_letter_grade_49_returns_F(self):
+        self.assertEqual("F", letter_grade(49))
