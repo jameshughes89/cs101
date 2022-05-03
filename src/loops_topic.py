c53c11c5ba2644ee5de745627c21b8c7220c2830
James Alexander Hughes <jameshughes89@Gmail.com>
2022-05-03 14:19:21 -0300

    mystery factorial to src

--- src/loops_topic.py
+++ src/loops_topic.py
@@ -1,9 +1,20 @@
-def trace_me_by_hand(n: int) -> int:
+def factorial(n: int) -> int:
     """
-    ????
+    Calculate factorial of n.
 
-    :param n:   ??
+    :param n:   Number to calculate factorial of
     :type n:    Integer
-    :return:    ??
+    :return:    The factorial of n
     :rtype:     Integer
     """
+    result = 1
+    while n > 1:
+        result = result * n
+        n -= 1
+    return result
+
+
+# Test trace_me_by_hand
+assert 1 == trace_me_by_hand(0)
+assert 1 == trace_me_by_hand(1)
+assert 3628800 == trace_me_by_hand(10)
