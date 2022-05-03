786ce3f7911f73f6cf29fd6757b9d562150837bd
James Alexander Hughes <jameshughes89@Gmail.com>
2022-05-03 15:31:04 -0300

    🎓 Loops Topic While Loops (#201)

---
+++ test/test_loops_topic.py
@@ -0,0 +1,14 @@
+import unittest
+
+from src.loops_topic import factorial
+
+
+class TestLoopsTopic(unittest.TestCase):
+    def test_factorial_0_returns_1(self):
+        self.assertEqual(1, factorial(0))
+
+    def test_factorial_1_returns_1(self):
+        self.assertEqual(1, factorial(1))
+
+    def test_factorial_10_returns_correct_number(self):
+        self.assertEqual(3628800, factorial(10))
