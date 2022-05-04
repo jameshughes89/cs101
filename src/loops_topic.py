9db9f4adffa4ab3c49da18238ca92ac267865124
James Alexander Hughes <jameshughes89@Gmail.com>
2022-05-04 09:32:36 -0300

    add int_sum

--- src/loops_topic.py
+++ src/loops_topic.py
@@ -18,3 +18,26 @@ def factorial(n: int) -> int:
 assert 1 == factorial(0)
 assert 1 == factorial(1)
 assert 3628800 == factorial(10)
+
+
+def int_sum(n: int) -> int:
+    """
+    Returns the sum of all numbers between 0 and n inclusively (be sure to include the value of n in the sum)
+
+    :param n: Number to sum to
+    :type n: Integer
+    :return: The sum of all numbers between 0 and n inclusively
+    :rtype: Integer
+    """
+    current_integer = 0
+    running_total = 0
+    while current_integer <= n:
+        running_total += current_integer
+        current_integer += 1
+    return running_total
+
+
+assert 0 == int_sum(0)
+assert 1 == int_sum(1)
+assert 55 == int_sum(10)
+assert 0 == int_sum(-1)
