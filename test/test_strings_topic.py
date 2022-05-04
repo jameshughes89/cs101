a3688c8ecd3a9f80673ab3aa7d70864acb81fc7c
James Alexander Hughes <jameshughes89@Gmail.com>
2022-05-04 11:21:44 -0300

    unittests

--- test/test_strings_topic.py
+++ test/test_strings_topic.py
@@ -2,7 +2,7 @@ import io
 import unittest
 import unittest.mock
 
-from src.strings_topic import vertical_print_for, vertical_print_while
+from src.strings_topic import vertical_print_for, vertical_print_while, character_is_in
 
 
 class TestStringsTopic(unittest.TestCase):
@@ -29,3 +29,23 @@ class TestStringsTopic(unittest.TestCase):
         vertical_print_for("Hello")
         expected = "H\ne\nl\nl\no\n"
         self.assertEqual(expected, mock_stdout.getvalue())
+
+    def test_character_is_in_a_in_empty_string_returns_false(self):
+        self.assertFalse(character_is_in("a", ""))
+
+    def test_character_is_in_empty_string_in_empty_string_returns_false(self):
+        self.assertFalse(character_is_in("", ""))
+
+    def test_character_is_in_empty_string_in_hello_string_returns_false(self):
+        self.assertFalse(character_is_in("", "hello"))
+
+    def test_character_is_in_no_matching_character_returns_false(self):
+        self.assertFalse(character_is_in("a", "hello"))
+
+    def test_character_is_in_first_character_match_returns_true(self):
+        self.assertTrue(character_is_in("h", "hello"))
+
+    def test_character_is_in_last_character_match_returns_true(self):
+        self.assertTrue(character_is_in("o", "hello"))
+
+        
