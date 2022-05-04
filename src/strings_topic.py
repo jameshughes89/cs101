d1899ff39c1684e42a7a5b4b27fc55c8ed4c7bb0
James Alexander Hughes <jameshughes89@Gmail.com>
2022-05-04 11:15:19 -0300

    character_is_in to src

--- src/strings_topic.py
+++ src/strings_topic.py
@@ -20,3 +20,30 @@ def vertical_print_for(a_string: str):
     """
     for char in a_string:
         print(char)
+
+
+def character_is_in(needle: str, haystack: str) -> bool:
+    """
+    Search through a string to determine if a given character is contained within it. Returns True if the character is
+    in the string and False otherwise.
+
+    :param needle:  Character to look for
+    :type needle: String
+    :param haystack: String to search through
+    :type haystack: String
+    :return: True if the character is in the string, false otherwise
+    :rtype: Boolean
+    """
+    for char in haystack:
+        if char == needle:
+            return True
+    return False
+
+
+# character_is_in tests
+assert False == character_is_in("a", "")
+assert False == character_is_in("", "")
+assert False == character_is_in("", "hello")
+assert False == character_is_in("a", "hello")
+assert True == character_is_in("h", "hello")
+assert True == character_is_in("o", "hello")
