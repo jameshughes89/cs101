26596e69ad3f99175013839a651dd5153e77a69a
James Alexander Hughes <jameshughes89@Gmail.com>
2022-05-05 10:02:31 -0300

    more assert tests

--- src/lists_topic.py
+++ src/lists_topic.py
@@ -22,6 +22,7 @@ def contains_while(needle, haystack) -> bool:
 
 # contains_while tests
 assert False == contains_while("a", [])
+assert False == contains_while([], [])
 assert False == contains_while("b", ["a", 1, 2.0, False])
 assert True == contains_while("a", ["a", 1, 2.0, False])
 assert True == contains_while(False, ["a", 1, 2.0, False])
@@ -29,9 +30,9 @@ assert True == contains_while(False, ["a", 1, 2.0, False])
 
 def index_of_for(needle, haystack) -> int:
     """
-    Linear search on a list. Return the index of the needle if it is found within haystack and -1 if it is not. Note
-    that type hints are not included in the definition of the function as we can search for things of arbitrary types
-    within a list containing a variety of arbitrary types.
+    Linear search on a list. Return the index of the first occurence of needle if it is found within haystack and -1 if
+    it is not. Note that type hints are not included in the definition of the function as we can search for things of
+    arbitrary types within a list containing a variety of arbitrary types.
 
     :param needle: Element to look for
     :type needle: Some arbitrary type
@@ -51,6 +52,7 @@ def index_of_for(needle, haystack) -> int:
 
 # index_of_for tests
 assert -1 == index_of_for("a", [])
+assert -1 == index_of_for([], [])
 assert -1 == index_of_for("b", ["a", 1, 2.0, False])
 assert 0 == index_of_for("a", ["a", 1, 2.0, False])
 assert 3 == index_of_for(False, ["a", 1, 2.0, False])
