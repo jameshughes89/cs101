66696623dee98e4dc4d4ab6fde6e420a1daf5a21
James Alexander Hughes <jameshughes89@Gmail.com>
2022-06-02 15:34:49 -0300

    pre Point class

--- src/point.py
+++ src/point.py
@@ -1,9 +1,23 @@
 import math
 
+
 class Point:
 
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
-    
+    def distance_from_origin(self) -> float:
+        pass
+
+    def distance_from_point(self, point) -> float:
+        pass
+
+    def find_midpoint(self, point):
+        pass
+
+    def __eq__(self, other):
+        pass
+
+    def __repr__(self):
+        pass
