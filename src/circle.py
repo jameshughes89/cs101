dee16765a823359acf1496a112f65ea0cf460466
James Alexander Hughes <jameshughes89@Gmail.com>
2022-06-02 10:40:48 -0300

    assert tests

--- src/circle.py
+++ src/circle.py
@@ -15,3 +15,16 @@ class Circle:
     def circumference(self) -> float:
         return 2 * math.pi * self.radius
 
+
+circle_zero = Circle(0)
+assert 0 == circle_zero.radius
+assert 0 == circle_zero.diameter()
+assert 0 == circle_zero.area()
+assert 0 == circle_zero.circumference()
+
+circle_10 = Circle(10)
+assert 10 == circle_10.radius
+assert 20 == circle_10.diameter()
+assert 0.001 < abs(circle_10.area() - 125.66)
+assert 0.001 < abs(circle_10.circumference() - 1256.64)
+
