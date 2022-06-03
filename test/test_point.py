b813d999416a6bc4f432f7c70cdb2b5014123c4b
James Alexander Hughes <jameshughes89@Gmail.com>
2022-06-03 09:04:59 -0300

    some more tests

--- test/test_point.py
+++ test/test_point.py
@@ -23,5 +23,23 @@ class TestCircle(unittest.TestCase):
         point.y = -1
         self.assertEqual(-1, point.y)
 
+    def test_distance_from_origin_of_point_0_0_returns_0(self):
+        point = Point(0, 0)
+        self.assertEqual(0, point.distance_from_origin())
+
+    def test_distance_from_origin_of_arbitrary_point_returns_correct_distance(self):
+        point = Point(1, 1)
+        self.assertAlmostEqual(1.41421356237, point.distance_from_origin())
+
+    def test_distance_from_point_from_point_to_same_point_returns_0(self):
+        point_from = Point(1, 1)
+        point_to = Point(1, 1)
+        self.assertEqual(0, point_from.distance_from_point(point_to))
+
+    def test_distance_from_point_from_arbitrary_point_to_arbitrary_point_returns_correct_distance(self):
+        point_from = Point(1, 1)
+        point_to = Point(-1, -1)
+        self.assertAlmostEqual(2.82842712475, point_from.distance_from_point(point_to))
+
 
 
