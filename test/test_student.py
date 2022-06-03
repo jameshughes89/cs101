fe96d44d687b4748efc68113846c3d1f01c800c5
James Alexander Hughes <jameshughes89@Gmail.com>
2022-06-03 16:29:16 -0300

    add attribute tests

--- test/test_student.py
+++ test/test_student.py
@@ -4,6 +4,18 @@ from src.student import Student
 
 
 class TestStudent(unittest.TestCase):
+    def test_student_first_name_returns_correct_first_name(self):
+        student = Student("Bob", "Smith", 123456789)
+        self.assertEqual("Bob", student.first_name)
+
+    def test_student_last_name_returns_correct_last_name(self):
+        student = Student("Bob", "Smith", 123456789)
+        self.assertEqual("Smith", student.last_name)
+
+    def test_student_student_number_returns_correct_student_number(self):
+        student = Student("Bob", "Smith", 123456789)
+        self.assertEqual(123456789, student.student_number)
+
     def test_equals_on_students_with_same_student_number_returns_true(self):
         student_a = Student("a", "student", 123456789)
         student_b = Student("b", "student", 123456789)
