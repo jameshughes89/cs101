ee62c1aa2276536b7384a43d777c47c1741d5557
James Alexander Hughes <jameshughes89@Gmail.com>
2022-06-06 13:34:06 -0300

    finish Course class

--- src/Course.py
+++ src/Course.py
@@ -8,4 +8,28 @@ class Course:
         self.course_name = course_name
         self._students = []
 
+    def size(self):
+        return len(self._students)
+
+    def add(self, student):
+        self._students.append(student)
+
+    def find(self, student):
+        for i, s in enumerate(self._students):
+            if s == student:
+                return i
+        return -1
+
+    def remove(self, student):
+        index = self.find(student)
+        if index == -1:
+            raise ValueError("No such student to remove")
+        else:
+            self._students.pop(index)
+
+    def __repr__(self):
+        s = ""
+        for student in self._students:
+            s += str(student)
+        return s
     
