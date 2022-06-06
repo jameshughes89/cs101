54d2af82829b167e4869fbe1b6e3e485f418cf63
James Alexander Hughes <jameshughes89@Gmail.com>
2022-06-06 14:18:54 -0300

    docstring and type hints

--- src/course.py
+++ src/course.py
@@ -8,26 +8,43 @@ class Course:
         self.course_name = course_name
         self._students = []
 
-    def size(self):
+    def size(self) -> int:
         return len(self._students)
 
     def add(self, student):
         self._students.append(student)
 
-    def find(self, student):
+    def find(self, student) -> int:
+        """
+        Returns the index of the first occurrence of a given Student if it exists. If it does not exist, return a
+        sentinel value of -1.
+
+        :param student: The student to search for
+        :type student: Student
+        :return: Index of the student, or -1 if it is not found
+        :rtype: int
+        """
         for i, s in enumerate(self._students):
             if s == student:
                 return i
         return -1
 
     def remove(self, student):
+        """
+        Remove the first occurence of the specified student from the collection. If the student does not exist, raise
+        an exception.
+
+        :raise Value error: If the student does not exist within the collection, a ValueError is raised.
+        :param student: The student to be removed
+        :type student: Student
+        """
         index = self.find(student)
         if index == -1:
             raise ValueError("No such student to remove")
         else:
             self._students.pop(index)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         s = ""
         for student in self._students:
             s += str(student) + "\n"
