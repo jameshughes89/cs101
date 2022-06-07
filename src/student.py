4b2617a49019117838abcec5e6c5a640af20bc52
James Alexander Hughes <jameshughes89@Gmail.com>
2022-06-07 11:47:43 -0300

    student class

--- src/student.py
+++ src/student.py
@@ -4,12 +4,12 @@ class Student:
     and checking equality.
     """
 
-    def __init__(self, first_name, last_name, student_number):
+    def __init__(self, first_name: str, last_name: str, student_number: int):
         self.first_name = first_name
         self.last_name = last_name
         self.student_number = student_number
 
-    def __eq__(self, other) -> bool:
+    def __eq__(self, other: "Student") -> bool:
         """
         Two students are considered equal if their student numbers are the same. This assumes student numbers are unique
         among students.
