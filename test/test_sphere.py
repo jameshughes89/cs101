1e6ccd315dd94d0d52b62447e840705b72efcfdd
James Alexander Hughes <jameshughes89@Gmail.com>
2022-06-15 11:28:47 -0300

    put kwargs back in actual rests

--- test/test_sphere.py
+++ test/test_sphere.py
@@ -22,7 +22,7 @@ class SphereTest(unittest.TestCase):
         ]
         expecteds = [0, 2, 0, 20.2]
         for (case, expect) in zip(cases, expecteds):
-            with self.subTest():
+            with self.subTest(case=case, expect=expect):
                 self.assertAlmostEqual(expect, case.diameter(), 5)
 
     def test_surface_area_various_spheres_returns_correct_surface_area(self):
@@ -34,7 +34,7 @@ class SphereTest(unittest.TestCase):
         ]
         expecteds = [0, 12.56637, 0, 1256.63706]
         for (case, expect) in zip(cases, expecteds):
-            with self.subTest():
+            with self.subTest(case=case, expect=expect):
                 self.assertAlmostEqual(expect, case.surface_area(), 5)
 
     def test_volume_various_spheres_returns_correct_volume(self):
@@ -46,7 +46,7 @@ class SphereTest(unittest.TestCase):
         ]
         expecteds = [0, 4.18879, 0, 4188.7902]
         for (case, expect) in zip(cases, expecteds):
-            with self.subTest():
+            with self.subTest(case=case, expect=expect):
                 self.assertAlmostEqual(expect, case.volume(), 5)
 
     def test_distance_between_centres_various_spheres_returns_correct_distance(self):
@@ -58,7 +58,7 @@ class SphereTest(unittest.TestCase):
         ]
         expecteds = [0, 1.732051, 0, 3.464102]
         for (case, expect) in zip(cases, expecteds):
-            with self.subTest():
+            with self.subTest(case=case, expect=expect):
                 self.assertAlmostEqual(expect, case[0].distance_between_centres(case[1]), 5)
 
     def test_distance_between_edges_various_spheres_returns_correct_distance(self):
@@ -70,7 +70,7 @@ class SphereTest(unittest.TestCase):
         ]
         expecteds = [-2, -0.267949, 15.320508, 8.052559]
         for (case, expect) in zip(cases, expecteds):
-            with self.subTest():
+            with self.subTest(case=case, expect=expect):
                 self.assertAlmostEqual(expect, case[0].distance_between_edges(case[1]), 5)
 
     def test_overlaps_various_spheres_returns_correct_boolean(self):
@@ -83,7 +83,7 @@ class SphereTest(unittest.TestCase):
         ]
         expecteds = [True, True, False, True, True]
         for (case, expect) in zip(cases, expecteds):
-            with self.subTest():
+            with self.subTest(case=case, expect=expect):
                 self.assertEqual(expect, case[0].overlaps(case[1]))
 
     def test_equals_on_equal_spheres_returns_true(self):
