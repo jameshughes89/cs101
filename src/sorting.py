437951b1917d83e61dacf749f0d633eede88348a
James Alexander Hughes <jameshughes89@Gmail.com>
2022-06-22 16:36:45 -0300

    add selection_sort code

---
+++ src/sorting.py
@@ -0,0 +1,12 @@
+def selection_sort(collection):
+    sorted_collection = []
+    for _ in collection:
+        current_smallest = collection[0]
+        for element in collection:
+            if element < current_smallest:
+                current_smallest = element
+        collection.remove(current_smallest)
+        sorted_collection.append(current_smallest)
+    return sorted_collection
+
+
