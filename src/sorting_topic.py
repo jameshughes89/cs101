89f74c2c1ad6ffe4dd88fca89949fcbf9ea073a8
James Alexander Hughes <jameshughes89@Gmail.com>
2022-06-24 14:17:02 -0300

    include insertion and bubble

--- src/sorting_topic.py
+++ src/sorting_topic.py
@@ -8,3 +8,35 @@ def selection_sort(collection):
         collection.remove(current_smallest)
         sorted_collection.append(current_smallest)
     return sorted_collection
+
+
+def insertion_sort(collection):
+    sorted_collection = []
+    for element in collection:
+        i = 0
+        # Scan sorted collection to find insertion spot
+        while i < len(sorted_collection) and sorted_collection[i] < element:
+            i += 1
+        sorted_collection.insert(i, element)
+    return sorted_collection
+
+
+def bubble_sort(collection):
+    for j in range(len(collection)):
+        for i in range(len(collection) - 1 - j):
+            if collection[i] > collection[i + 1]:
+                collection[i], collection[i + 1] = collection[i + 1], collection[i]
+    return collection
+
+
+def bubble_sort_improved(collection):
+    has_swapped = True
+    complete_cells = 0
+    while has_swapped:
+        has_swapped = False
+        for i in range(len(collection) - 1 - complete_cells):
+            if collection[i] > collection[i + 1]:
+                collection[i], collection[i + 1] = collection[i + 1], collection[i]
+                has_swapped = True
+        complete_cells += 1
+    return collection
