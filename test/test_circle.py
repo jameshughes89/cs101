b44ebaabda8a0142494bd977be0c7112f6d7ce43
James Hughes <jameshughes89@gmail.com>
2022-11-17 16:10:21 -0400

    remove setUp from circle tests

--- test/test_circle.py
+++ test/test_circle.py
@@ -4,33 +4,38 @@ from src.circle import Circle
 
 
 class CircleTest(unittest.TestCase):
-    def setUp(self):
-        self.circle_0 = Circle(0)
-        self.circle_10 = Circle(10)
 
     def test_circle_of_radius_0_has_radius_0(self):
-        self.assertEqual(0, self.circle_0.radius)
+        circle_0 = Circle(0)
+        self.assertEqual(0, circle_0.radius)
 
     def test_circle_of_radius_0_has_diameter_0(self):
-        self.assertEqual(0, self.circle_0.diameter())
+        circle_0 = Circle(0)
+        self.assertEqual(0, circle_0.diameter())
 
     def test_circle_of_radius_0_has_area_0(self):
-        self.assertEqual(0, self.circle_0.area())
+        circle_0 = Circle(0)
+        self.assertEqual(0, circle_0.area())
 
     def test_circle_of_radius_0_has_circumference_0(self):
-        self.assertEqual(0, self.circle_0.circumference())
+        circle_0 = Circle(0)
+        self.assertEqual(0, circle_0.circumference())
 
     def test_circle_of_radius_10_has_radius_10(self):
-        self.assertEqual(10, self.circle_10.radius)
+        circle_10 = Circle(10)
+        self.assertEqual(10, circle_10.radius)
 
     def test_circle_of_radius_10_has_correct_diameter(self):
-        self.assertEqual(20, self.circle_10.diameter())
+        circle_10 = Circle(10)
+        self.assertEqual(20, circle_10.diameter())
 
     def test_circle_of_radius_10_has_correct_area(self):
-        self.assertAlmostEqual(314.159265, self.circle_10.area(), 5)
+        circle_10 = Circle(10)
+        self.assertAlmostEqual(314.159265, circle_10.area(), 5)
 
     def test_circle_of_radius_10_has_correct_circumference(self):
-        self.assertAlmostEqual(62.831853, self.circle_10.circumference(), 5)
+        circle_10 = Circle(10)
+        self.assertAlmostEqual(62.831853, circle_10.circumference(), 5)
 
     def test_setting_radius_to_20_updates_radius_to_20(self):
         self.circle_0.radius = 20
