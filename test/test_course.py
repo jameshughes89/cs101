ffa0d98ccad8c6781ea379ab4ef35ef773578b41
YourLocalMedic <76868965+YourLocalMedic@users.noreply.github.com>
2024-11-29 13:29:28 -0400

    Update test_course.py to add tests for zero students (#654)

--- test/test_course.py
+++ test/test_course.py
@@ -23,6 +23,11 @@ class CourseTest(TestCase):
         student = Student("Bob", "Smith", 123456789)
         with self.assertRaises(ValueError):
             course.remove(student)
+            
+    def test_contains_empty_course_returns_false(self):
+        course = Course("CS101")
+        student_not_there = Student("Not", "There", 918273645)
+        self.assertFalse(course.contains(student_not_there))
 
     def test_repr_empty_course_returns_empty_string(self):
         course = Course("CS101")
