edacf7ba219bcec31f772986a60ad854bce868c9
JamesHughes89 <JamesHughes89@Gmail.com>
2026-04-21 10:57:52 -0300

    rename project scripts

--- src/console_scripts/cli.py
+++ src/console_scripts/cli.py
@@ -1,12 +1,12 @@
 import subprocess
 
-def run_code_formatters():
+def run_formatters():
     for tool in ["isort .", "black .", "mdformat ."]:
         print(f"running `{tool}`")
         subprocess.run(tool, shell=True)
 
 
-def run_code_verification():
+def run_verification():
     tool = "flake8 src/ test/"
     print(f"running `{tool}`")
     subprocess.run(tool, shell=True)
