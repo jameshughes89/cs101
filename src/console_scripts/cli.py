d1655ca8461bb1aed95fe4d18b0fd5c022c67f2e
JamesHughes89 <JamesHughes89@Gmail.com>
2026-04-23 10:43:49 -0300

    add linkcheck project script

--- src/console_scripts/cli.py
+++ src/console_scripts/cli.py
@@ -13,6 +13,12 @@ def run_verification():
         subprocess.run(tool, shell=True)
 
 
+def run_linkcheck():
+    tool = "sphinx-build -b linkcheck site/ out/"
+    print(f"running `{tool}`")
+    subprocess.run(tool, shell=True)
+
+
 def run_sphinx_build():
     tool = "sphinx-build -b html site/ out/"
     print(f"running `{tool}`")
